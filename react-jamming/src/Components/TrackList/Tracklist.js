3f7be1bfe7cab933c814c9af48ca90ecf363228f
Jeff Garrett <jeff.garrett32@gmail.com>
2022-02-06 11:39:12 -0500

    Sections 1 and 2

---
+++ react-jamming/src/Components/TrackList/Tracklist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Tracklist.css';
+
+class Tracklist extends React.Component {
+    render () {
+        return (
+            <div class="TrackList">
+                <Tracklist />
+            </div>
+        )
+    }
+}
+
+export default Tracklist;
